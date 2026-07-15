87c63f7357d8fc804259523c72a960391af9527f
Senku24 <nixonpaule24@gmail.com>
2026-07-15 06:15:41 +0530

    inprogress of defining routes

--- index.js
+++ index.js
@@ -44,8 +44,20 @@ app.post('/household', authmiddleware, async (req, res) => {
     })
     res.status(201).json({ message: "Household created successfully", household: newHousehold._id });
 });
-app.post('/household/:join', authmiddleware, async (req, res) => {
+app.post('/household/join', authmiddleware, async (req, res) => {
     const userId = req.userId;
+    const inviteCode = req.body.inviteCode;
+
+    const house = await householdModel.findOne({ inviteCode });
+    if(!house) {
+        return res.status(404).json({ message: "Household not found" });
+    }
+    if(house.members.includes(userId)) {
+        return res.status(400).json({ message: "User already a member of this household" });
+    }
+    const addmember = await householdModel.updateOne({_id: house._id}, { $addToSet: { members: userId } });
+    const updateUser = await userModel.updateMany({_id: userId}, { $set: { householdId: house._id } });
+    res.status(200).json({ message: "Joined household successfully" });
 });
 // read endpoints
 // update endpoints
